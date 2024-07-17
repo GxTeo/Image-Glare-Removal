c57729776bfee7b15db9b8aaf1673e1bd04794cd
GxTeo <teog0015@e.ntu.edu.sg>
2024-07-17 12:45:45 +0800

    pytest to test endpoints

--- script/test_endpoint.py
+++ script/test_endpoint.py
@@ -1,38 +1,15 @@
+import pytest
 import requests
 from PIL import Image
 import io
 import base64
 
+
 url = "http://127.0.0.1:4000"
-
-def test_ping():
-    response = requests.get(f"{url}/ping")
-    if response.status_code != 200:
-        print("Ping failed")
-        return
-    assert response.json().get("message") == "pong"
-    print("Ping successful")
-    return
-
-def test_infer(image_path):
-    files = {"image": open(image_path, "rb")}
-    response = requests.post(f"{url}/infer", files=files)
-
-    if response.status_code != 200:
-        print("Inference failed")
-        return
-    
-    image_binary = base64.b64decode(response.json().get("image"))
-    image = Image.open(io.BytesIO(image_binary))
-    image.save("predicted_image.png")
-    print("Image saved as predicted_image.png")
-    return
-
-
-if __name__ == "__main__":
-    test_ping()
-
-    # Given that we are using the image from the validation dataset, we need to crop out the glare image
+@pytest.fixture()
+def image_path():
+    # Given that we are using the image from the validation dataset, 
+    # we need to crop out the glare image
     image_path = "../SD1/val/006.png"
     image = Image.open(image_path)
 
@@ -42,4 +19,26 @@ if __name__ == "__main__":
 
     # Save the glare image
     glare_image.save("test_glare_image.png")
-    test_infer("test_glare_image.png")
+    return "test_glare_image.png"
+
+
+def test_ping():
+    response = requests.get(f"{url}/ping")
+    assert response.status_code == 200, "Ping failed"
+    assert response.json().get("message") == "pong", "Ping response is not 'pong'"
+    print("Ping successful")
+
+
+def test_infer(image_path):
+    files = {"image": open(image_path, "rb")}
+    response = requests.post(f"{url}/infer", files=files)
+
+    assert response.status_code == 200, "Inference failed"
+    image_binary = base64.b64decode(response.json().get("image"))
+    image = Image.open(io.BytesIO(image_binary))
+    image.save("predicted_image.png")
+    print("Image saved as predicted_image.png")
+
+
+if __name__ == "__main__":
+    pytest.main()
